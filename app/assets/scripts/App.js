5e9694ed1c1c509df2953268919b807753195f04
mhshimul85 <ishemul85@gmail.com>
2019-01-07 03:39:17 +0600

    Test

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,4 @@
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
+import Navigation from './modules/Navigation';
 import $ from 'jquery';
-import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
 
-var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".feature-item"), "85%");
-new RevealOnScroll($(".testimonial"), "60%");
-var stickyHeader = new StickyHeader();
-var modal = new Modal();
+var navigation = new Navigation();
---
+++ app/assets/scripts/modules/Navigation.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+
+class Navigation {
+    constructor() {        
+        alert('');
+    }
+}
+
+export default Navigation;
+/*
+
+$(document).ready(function(){
+
+    $('.navbar-icon').click(function(){
+      $(this).toggleClass('change');
+      $('nav > ul').toggleClass('mh-show');
+    });
+  
+    $('li:has(ul)').children('a').append('<i></i>');
+      
+    $('li > a').each(function(){
+      $(this).click(function(){
+        $(this).next().toggleClass('mh-show');
+        $(this).children().toggleClass('mh-up');
+      });    
+    });
+  });
